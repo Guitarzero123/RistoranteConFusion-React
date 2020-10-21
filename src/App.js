463cd0faf59bdce9266aaa4a7cc574e33a5530ba
Matthew Little <m.a.litle94@gmail.com>
2020-10-20 23:49:31 -0400

    Updated Menu Component with Cards

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import  React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap'
 import Menu from './components/MenuComponent'
 import './App.css';
+import { DISHES } from './shared/dishes'
 
 //App as Class
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       <div>
@@ -15,7 +25,7 @@ class App extends Component {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={this.state.dishes} />
     </div>
     );
   }
