2830d4c9619c1aefa2f7b1a999139e242d8b96c3
Matthew Little <m.a.litle94@gmail.com>
2020-10-22 10:08:29 -0400

    Added Contact Component. Updated routing and DishDetail

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle, ListGroup, ListGroupItem } from 'reactstrap'; 
+import { Card, CardImg, CardText, CardBody, CardTitle, ListGroup, ListGroupItem, Breadcrumb, BreadcrumbItem } from 'reactstrap'; 
+import { Link } from 'react-router-dom';
 
 
 function RenderDish({ dish }) {
@@ -45,16 +46,23 @@ function RenderComments({ comments }) {
 }
 
 function DishDetail(props) {
-    const dish = props.dish;
-    const comments = dish != null ? dish.comments : null;
         return (
             <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to={'/menu'}>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                </div>
                 <div className="row"> 
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={dish} />
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={comments} />
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
             </div> 
