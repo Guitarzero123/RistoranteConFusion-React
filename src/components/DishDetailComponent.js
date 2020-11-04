b9e5d3046c4c095c015edf9bbce98e2364e56487
Matthew Little <m.a.litle94@gmail.com>
2020-11-04 08:48:28 -0500

    Created first redux action 'addComments'

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -27,8 +27,8 @@ class CommentForm extends Component {
     }
 
     handleSubmit(values) {
-        console.log("Current state is:" + JSON.stringify(values));
-        alert("Current state is:" + JSON.stringify(values));
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
 
     render() {
@@ -103,7 +103,7 @@ function RenderDish({ dish }) {
     }
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, dishId }) {
     if(comments != null) {
 
         const commentList = comments.map((comment) => {
@@ -121,7 +121,7 @@ function RenderComments({ comments }) {
                 <ListGroup>
                     {commentList}
                 </ListGroup>
-                <CommentForm />
+                <CommentForm dishId={dishId} addComment={addComment} />
             </div>
         );
         
@@ -149,7 +149,9 @@ function DishDetail(props) {
                         <RenderDish dish={props.dish} />
                     </div>
                     <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
+                        <RenderComments comments={props.comments} 
+                            addComment={props.addComment} 
+                            dishId={props.dish.id} />
                     </div>
                 </div>
             </div> 
