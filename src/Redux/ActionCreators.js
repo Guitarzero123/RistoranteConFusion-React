03b892557f8379a20f7a14af935a00dba3273e19
Matthew Little <m.a.litle94@gmail.com>
2021-01-11 13:01:17 -0500

    fetch error handling added to action creators

--- src/Redux/ActionCreators.js
+++ src/Redux/ActionCreators.js
@@ -17,8 +17,22 @@ export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
     
   return fetch(baseUrl + 'dishes')
+    .then(response => {
+        if(response.ok) {
+            return response;
+        } else {
+            let error = new Error(`Error ${response.status}: ${response.statusText}`);
+            error.response = response;
+            throw error;
+        }
+    },
+    error => {
+        let errorMessage = new Error(error.message);
+        throw errorMessage;
+    })
     .then(response => response.json())
-    .then(dishes => dispatch(addDishes(dishes)));
+    .then(dishes => dispatch(addDishes(dishes)))
+    .catch(error => dispatch(dishesFailed(error.message)));
 };
 
 //tells fetchDishes if dishes are being loaded
@@ -42,8 +56,22 @@ export const addDishes = (dishes) => ({
 export const fetchComments = () => (dispatch) => {
     
   return fetch(baseUrl + 'comments')
+    .then(response => {
+        if(response.ok) {
+            return response;
+        } else {
+            let error = new Error(`Error ${response.status}: ${response.statusText}`);
+            error.response = response;
+            throw error;
+        }
+    },
+    error => {
+        let errorMessage = new Error(error.message);
+        throw errorMessage;
+    })
     .then(response => response.json())
-    .then(comments => dispatch(addComments(comments)));
+    .then(comments => dispatch(addComments(comments)))
+    .catch(error => dispatch(commentsFailed(error.message)));
 };
 
 //tells website comments failed loading
@@ -63,8 +91,22 @@ export const fetchPromos = () => (dispatch) => {
     dispatch(promosLoading(true));
 
     return fetch(baseUrl + 'promotions')
+        .then(response => {
+            if(response.ok) {
+                return response;
+            } else {
+                let error = new Error(`Error ${response.status}: ${response.statusText}`);
+                error.response = response;
+                throw error;
+            }
+        },
+        error => {
+            let errorMessage = new Error(error.message);
+            throw errorMessage;
+        })
       .then(response => response.json())
-      .then(promos => dispatch(addPromos(promos)));
+      .then(promos => dispatch(addPromos(promos)))
+      .catch(error => dispatch(promosFailed(error.message)));
 };
 
 //tells fetchPromos if promos are being loaded
